94adbedec1e0af75de28075a27126349364dbbdd
Dzmitry Liavonau <dleonov701@gmail.com>
2026-09-20 10:45:44 +0300

    Добавил формулу аннуитетного платежа и построение графика платежей с точным до копейки последним платежом.

---
+++ CreditCalculator.Calculations/AnnuityScheduleCalculator.cs
@@ -0,0 +1,61 @@
+namespace CreditCalculator.Calculations;
+
+public static class AnnuityScheduleCalculator
+{
+    public static decimal CalculateMonthlyPayment(decimal principal, decimal annualRatePercent, int termMonths)
+    {
+        var monthlyRate = annualRatePercent / 12 / 100;
+
+        if (monthlyRate == 0m)
+            return Math.Round(principal / termMonths, 2, MidpointRounding.AwayFromZero);
+
+        var factor = 1m - 1m / DecimalPow(1m + monthlyRate, termMonths);
+        return Math.Round(principal * monthlyRate / factor, 2, MidpointRounding.AwayFromZero);
+    }
+
+    public static PaymentScheduleResult BuildSchedule(decimal principal, decimal annualRatePercent, int termMonths, DateOnly firstPaymentDate)
+    {
+        var monthlyRate = annualRatePercent / 12 / 100;
+        var payment = CalculateMonthlyPayment(principal, annualRatePercent, termMonths);
+
+        var payments = new List<PaymentScheduleItem>(termMonths);
+        var balance = principal;
+
+        for (var number = 1; number <= termMonths; number++)
+        {
+            var date = firstPaymentDate.AddMonths(number - 1);
+            var interest = Math.Round(balance * monthlyRate, 2, MidpointRounding.AwayFromZero);
+
+            decimal principalPart;
+            decimal paymentAmount;
+
+            if (number == termMonths)
+            {
+                principalPart = balance;
+                paymentAmount = principalPart + interest;
+            }
+            else
+            {
+                principalPart = payment - interest;
+                paymentAmount = payment;
+            }
+
+            balance -= principalPart;
+            payments.Add(new PaymentScheduleItem(number, date, paymentAmount, interest, principalPart, balance));
+        }
+
+        var totalPaid = payments.Sum(item => item.Payment);
+        var overpayment = totalPaid - principal;
+
+        return new PaymentScheduleResult(payments, totalPaid, overpayment);
+    }
+
+    private static decimal DecimalPow(decimal value, int exponent)
+    {
+        var result = 1m;
+        for (var i = 0; i < exponent; i++)
+            result *= value;
+
+        return result;
+    }
+}
---
+++ tests/CreditCalculator.Calculations.Tests/AnnuityScheduleCalculatorTests.cs
@@ -0,0 +1,58 @@
+using CreditCalculator.Calculations;
+using FluentAssertions;
+
+namespace CreditCalculator.Calculations.Tests;
+
+public class AnnuityScheduleCalculatorTests
+{
+    [Fact]
+    public void BuildSchedule_ShortTerm_MatchesReferenceValues()
+    {
+        var result = AnnuityScheduleCalculator.BuildSchedule(20000m, 18m, 24, new DateOnly(2026, 1, 1));
+
+        result.Payments.Should().HaveCount(24);
+        result.Payments[0].Payment.Should().Be(998.48m);
+        result.TotalPaid.Should().Be(23963.59m);
+        result.Overpayment.Should().Be(3963.59m);
+        result.Payments[^1].RemainingBalance.Should().Be(0m);
+    }
+
+    [Fact]
+    public void BuildSchedule_LongTerm_MatchesReferenceValues()
+    {
+        var result = AnnuityScheduleCalculator.BuildSchedule(150000m, 12m, 180, new DateOnly(2026, 1, 1));
+
+        result.Payments.Should().HaveCount(180);
+        result.Payments[0].Payment.Should().Be(1800.25m);
+        result.TotalPaid.Should().Be(324046.05m);
+        result.Overpayment.Should().Be(174046.05m);
+        result.Payments[^1].RemainingBalance.Should().Be(0m);
+    }
+
+    [Fact]
+    public void CalculateMonthlyPayment_ZeroRate_ReturnsPrincipalDividedByTerm()
+    {
+        var payment = AnnuityScheduleCalculator.CalculateMonthlyPayment(12000m, 0m, 12);
+
+        payment.Should().Be(1000m);
+    }
+
+    [Fact]
+    public void BuildSchedule_ZeroRate_HasNoInterestAndNoKopeckTail()
+    {
+        var result = AnnuityScheduleCalculator.BuildSchedule(12000m, 0m, 12, new DateOnly(2026, 1, 1));
+
+        result.Payments.Should().OnlyContain(item => item.InterestPart == 0m);
+        result.TotalPaid.Should().Be(12000m);
+        result.Overpayment.Should().Be(0m);
+        result.Payments[^1].RemainingBalance.Should().Be(0m);
+    }
+
+    [Fact]
+    public void BuildSchedule_RemainingBalance_NeverGoesNegative()
+    {
+        var result = AnnuityScheduleCalculator.BuildSchedule(20000m, 18m, 24, new DateOnly(2026, 1, 1));
+
+        result.Payments.Should().OnlyContain(item => item.RemainingBalance >= 0m);
+    }
+}
